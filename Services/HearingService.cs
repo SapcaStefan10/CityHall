b563eb0509a70f62fc48fd66f6143de9f8367f14
Stefan Sapca <sapca.stefan10@gmail.com>
2026-05-13 18:03:29 +0300

    Services (#9)
    * Controller Backend
    
    * Added service interfaces
    
    * WIP services
    
    * Services fixed
    
    ---------
    
    Co-authored-by: Stefan Sapca <sapca.stefan@gmail.com>

---
+++ Services/HearingService.cs
@@ -0,0 +1,35 @@
+﻿using SElab5.Models;
+using SElab5.Repositories.Interfaces;
+using SElab5.Services.Interfaces;
+
+namespace SElab5.Services
+{
+    public class HearingService : IHearingService
+    {
+        private readonly IHearingRepository _hearingRepository;
+
+        public HearingService(IHearingRepository hearingRepository)
+        {
+            _hearingRepository = hearingRepository;
+        }
+
+        public async Task<IEnumerable<Hearing>> GetUserHearingsAsync(int userId)
+        {
+            return await _hearingRepository.GetByCitizenIdAsync(userId);
+        }
+
+        public async Task<IEnumerable<Hearing>> GetAllHearingsAsync()
+        {
+            return await _hearingRepository.GetAllAsync();
+        }
+
+        public async Task<bool> ScheduleHearingAsync(Hearing hearing)
+        {
+            hearing.CreatedAt = DateTime.Now;
+            hearing.Status = HearingStatus.Scheduled;
+            await _hearingRepository.AddAsync(hearing);
+            return await _hearingRepository.SaveChangesAsync() > 0;
+        }
+    }
+}
+
